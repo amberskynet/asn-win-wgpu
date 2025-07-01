7c4440c9632e16b6fab03ef468b4fd6c72883af1
sir Random <random@amberskynet.org>
2025-07-01 13:33:45 +0300

    little refactoring

--- examples/ex_1.rs
+++ examples/ex_1.rs
@@ -1,7 +1,15 @@
+extern crate asn_logger;
 extern crate asn_win_wgpu;
 
+use asn_logger::{init_log, AsnLogConfig, AsnLogLevel};
 use asn_win_wgpu::run;
 
 fn main() {
+    let c = AsnLogConfig {
+        global_level: AsnLogLevel::Trace,
+        module_levels: Default::default(),
+    };
+    init_log(&c);
+
     run()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,15 @@
+mod app;
 mod data;
-mod winit_app;
 
 extern crate asn_logger;
 extern crate winit;
 
-use asn_logger::{info, init_log, warn, AsnLogConfig, AsnLogLevel};
+use app::App;
+use asn_logger::{error, info};
 use data::LOG_MODULE_NAME;
 use winit::event_loop::{ControlFlow, EventLoop};
-use winit_app::App;
 
 pub fn run() {
-    let c = AsnLogConfig {
-        global_level: AsnLogLevel::Trace,
-        module_levels: Default::default(),
-    };
-    init_log(&c);
-
     info(LOG_MODULE_NAME, "try run");
 
     let event_loop = EventLoop::new().unwrap();
@@ -29,6 +23,6 @@ pub fn run() {
 
     match result {
         Ok(_) => info(LOG_MODULE_NAME, "exit successful"),
-        Err(e) => {}
+        Err(e) => error(LOG_MODULE_NAME, e.to_string().as_str()),
     }
 }
