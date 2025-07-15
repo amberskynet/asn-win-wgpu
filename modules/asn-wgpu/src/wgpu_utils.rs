03c536d5181af60d8e327acea00a2b2ca78abe43
mrstreamer963 <698lo7epy@mozmail.com>
2025-07-15 10:14:16 +0300

    update after cursor

--- modules/asn-wgpu/src/wgpu_utils.rs
+++ modules/asn-wgpu/src/wgpu_utils.rs
@@ -1,5 +1,6 @@
 use crate::vertex::Vertex;
 
+use wgpu::util::DeviceExt;
 /// Создает рендер пайплайн для GPU
 ///
 /// # Arguments
@@ -38,7 +39,6 @@ pub fn get_render_pipeline(
             module: &shader,
             entry_point: Some("fs_main"),
             targets: &[Some(wgpu::ColorTargetState {
-                // 4.
                 format,
                 blend: Some(wgpu::BlendState::REPLACE),
                 write_mask: wgpu::ColorWrites::ALL,
@@ -65,4 +65,102 @@ pub fn get_render_pipeline(
     });
 
     render_pipeline
+}
+
+pub fn get_texture(
+    device: &wgpu::Device,
+    config: &wgpu::SurfaceConfiguration,
+    size: wgpu::Extent3d,
+    queue: &wgpu::Queue,
+    format: wgpu::TextureFormat,
+    usage: wgpu::TextureUsages,
+) -> wgpu::Texture {
+    let diffuse_bytes = include_bytes!("happy-tree.png");
+    let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+    let diffuse_rgba = diffuse_image.to_rgba8();
+
+    use image::GenericImageView;
+    let dimensions = diffuse_image.dimensions();
+
+    let texture_size = wgpu::Extent3d {
+        width: dimensions.0,
+        height: dimensions.1,
+        // All textures are stored as 3D, we represent our 2D texture
+        // by setting depth to 1.
+        depth_or_array_layers: 1,
+    };
+
+    let diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
+        size: texture_size,
+        mip_level_count: 1, // We'll talk about this a little later
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        // Most images are stored using sRGB, so we need to reflect that here.
+        format: wgpu::TextureFormat::Rgba8UnormSrgb,
+        // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
+        // COPY_DST means that we want to copy data to this texture
+        usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+        label: Some("diffuse_texture"),
+        // This is the same as with the SurfaceConfig. It
+        // specifies what texture formats can be used to
+        // create TextureViews for this texture. The base
+        // texture format (Rgba8UnormSrgb in this case) is
+        // always supported. Note that using a different
+        // texture format is not supported on the WebGL2
+        // backend.
+        view_formats: &[],
+    });
+
+    queue.write_texture(
+        // Tells wgpu where to copy the pixel data
+        wgpu::TexelCopyTextureInfo {
+            texture: &diffuse_texture,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+            aspect: wgpu::TextureAspect::All,
+        },
+        // The actual pixel data
+        &diffuse_rgba,
+        // The layout of the texture
+        wgpu::TexelCopyBufferLayout {
+            offset: 0,
+            bytes_per_row: Some(4 * dimensions.0),
+            rows_per_image: Some(dimensions.1),
+        },
+        texture_size,
+    );
+
+    let buffer = device.create_buffer_init(
+        &wgpu::util::BufferInitDescriptor {
+            label: Some("Temp Buffer"),
+            contents: &diffuse_rgba,
+            usage: wgpu::BufferUsages::COPY_SRC,
+        }
+    );
+
+    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("texture_buffer_copy_encoder"),
+    });
+
+    encoder.copy_buffer_to_texture(
+        wgpu::TexelCopyBufferInfo {
+            buffer: &buffer,
+            layout: wgpu::TexelCopyBufferLayout {
+                offset: 0,
+                bytes_per_row: Some(4 * dimensions.0),
+                rows_per_image: Some(dimensions.1),
+            },
+        },
+        wgpu::TexelCopyTextureInfo {
+            texture: &diffuse_texture,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+            aspect: wgpu::TextureAspect::All,
+        },
+        texture_size,
+    );
+
+    queue.submit(std::iter::once(encoder.finish()));
+
+    diffuse_texture
 }
