8fc91c19b56ba62c7d297f2d75d6a944c2595894
mrstreamer963 <698lo7epy@mozmail.com>
2025-07-28 08:55:19 +0300

    update after cursir ai

--- modules/asn-wgpu/src/lib.rs
+++ modules/asn-wgpu/src/lib.rs
@@ -3,11 +3,12 @@ pub mod state_error;
 
 mod data;
 mod texture;
-mod wgpu_components;
+pub mod wgpu_components;
 mod wgpu_utils;
 
 pub use state::State;
 pub use state_error::StateError;
 
+pub use wgpu_components::wgpu_map::RgbaHandler;
 pub use wgpu_components::wgpu_mesh_color::WgpuQuad;
 pub use wgpu_components::wgpu_mesh_textured::WgpuQuadTextured;
