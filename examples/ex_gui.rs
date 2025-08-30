14931d01e51158ed157c5dd58b53c254288f6136
mrstreamer963 <698lo7epy@mozmail.com>
2025-08-30 20:48:25 +0300

    it working with help of qwen3

--- examples/ex_gui.rs
+++ examples/ex_gui.rs
@@ -19,7 +19,7 @@ use log_utils::setup_log;
 
 pub const LOG_MODULE_NAME: &str = "ex_gui";
 
-pub const MAP: [u32; 4] = [1, 1, 1, 1];
+pub const MAP: [u32; 4] = [1, 2, 3, 4];
 
 pub struct GuiList {
     m: WgpuMap,
