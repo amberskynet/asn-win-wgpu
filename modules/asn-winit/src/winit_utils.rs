31a6e5e440b94b8050ed978f851b23240eb10a0a
mrstreamer963 <698lo7epy@mozmail.com>
2025-09-06 14:13:42 +0300

    some updates

--- modules/asn-winit/src/winit_utils.rs
+++ modules/asn-winit/src/winit_utils.rs
@@ -1,5 +1,6 @@
 use asn_gui_core::AsnGuiWindowConfig;
-use asn_logger::m_error;
+#[allow(unused_imports)]
+use asn_logger::{m_error, m_info};
 
 use crate::data::LOG_MODULE_NAME;
 use crate::error::{AsnWinitError, window_creation_error};
@@ -41,6 +42,7 @@ pub fn new_window(
         let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw();
         let html_canvas_element = canvas.unchecked_into();
         window_attributes = window_attributes.with_canvas(Some(html_canvas_element));
+        m_info!("window_attributes: {:?}", window_attributes);
     }
 
     let window = match event_loop.create_window(window_attributes) {
