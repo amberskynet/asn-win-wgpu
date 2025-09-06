a7b9313a6df141cbaa9eabc0a4603d766fe2a098
mrstreamer963 <698lo7epy@mozmail.com>
2025-09-06 23:11:23 +0300

    some update

--- examples/ex_web/src/lib.rs
+++ examples/ex_web/src/lib.rs
@@ -1,4 +1,5 @@
 use asn_logger::log::info;
+mod gui;
 mod gui_empty;
 mod gui_wgpu;
 mod log_utils;
@@ -10,7 +11,8 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 use web_sys::console;
 
-use crate::gui_wgpu::run_gui;
+use crate::gui::run_gui;
+// use crate::gui_wgpu::run_gui;
 // use crate::gui_empty::run_gui;
 
 #[wasm_bindgen]
