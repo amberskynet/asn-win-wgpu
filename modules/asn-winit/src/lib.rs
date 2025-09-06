a8e274b594c70875518ffb6a48c25a1b130c9693
mrstreamer963 <698lo7epy@mozmail.com>
2025-09-06 08:39:10 +0300

    move run to web

--- modules/asn-winit/src/lib.rs
+++ modules/asn-winit/src/lib.rs
@@ -8,7 +8,9 @@ mod keyboard_handler;
 use asn_gui_core::TAsnRenderManager;
 use asn_logger::*;
 use data::LOG_MODULE_NAME;
-use winit::event_loop::ControlFlow;
+#[cfg(target_arch = "wasm32")]
+use winit::error::EventLoopError;
+use winit::event_loop::{ControlFlow, EventLoop};
 
 mod app_state;
 mod winit_utils;
@@ -35,7 +37,7 @@ where
 {
     m_info!("run()");
 
-    let event_loop = winit::event_loop::EventLoop::<UserEvents<R>>::with_user_event()
+    let event_loop = EventLoop::<UserEvents<R>>::with_user_event()
         .build()
         .map_err(|e| event_loop_creation_error(format!("Failed to create event loop: {e}")))?;
 
@@ -44,7 +46,18 @@ where
     let mut runner = new_state(r, proxy);
 
     event_loop.set_control_flow(ControlFlow::Poll);
+
+    #[cfg(not(target_arch = "wasm32"))]
     let result = event_loop.run_app(&mut runner);
+
+    #[cfg(target_arch = "wasm32")]
+    let result: Result<(), EventLoopError> = {
+        use winit::platform::web::EventLoopExtWebSys;
+        wasm_bindgen_futures::spawn_local(async move {
+            event_loop.spawn_app(runner);
+        });
+        Ok(())
+    };
 
     match result {
         Ok(_) => {
